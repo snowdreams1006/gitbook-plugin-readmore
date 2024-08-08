4ea9f512821a8449d08842ea4999f047101b3cf8
snowdreams1006 <snowdreams1006@163.com>
2024-08-08 14:13:27 +0800

    auto update

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ module.exports = {
                 <div id="container">
                     ${page.content}
                 </div>
+                <script src="https://readmore.openwrite.cn/js/readmore-2.0.js" type="text/javascript"></script>
                 <script src="https://blog.snowdreams1006.cn/assets/js/readmore.js" type="text/javascript"></script>
                 <script>
-                    let btw = new BTWPlugin();
-                    let readmoreConfig = ${JSON.stringify(readmoreConfig)};
-                    btw.init(readmoreConfig);
-                </script>`;
+                    var btw = new BTWPlugin();
+                    btw.init(${JSON.stringify(readmoreConfig)});
+                    (function anonymous(
+                    ) {
+                    debugger
+                    })
+                </script>
+                `;
 
                 page.content = str;
             }
